ae1f80b5e8f7dd4c22a6904fb48db8a72760d05d
Yash-pfj <Yash.Vashisth@pilottravelcenters.com>
2023-08-02 19:54:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import io from 'socket.io-client'
+import Chat from './Chat';
+
+const socket = io.connect("https://node-server-test-one.vercel.app/")
+socket.on("notification",(data)=> console.log('testing notification data',data))
+socket.on("send_message",(data)=> console.log('testing send_message',data))
 
 function App() {
+
+  const [username,setUsername] = useState("");
+  const [room,setRoom] = useState("")
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Join A Chat</h3>
+     <input type="text" placeholder="John..." onChange={(e) => setUsername(e.target.value)}/>
+     <input type="text" placeholder='RoomId' onChange={(e)=> setRoom(e.target.value)}/>
+     <button onClick={joinRoom}>Join A Room</button>
+
+     <Chat socket={socket} username={username} room={room} />
     </div>
   );
 }
---
+++ src/Chat.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+function Chat({ socket, userName, room }) {
+    const [currentMessage, setCurrentMessage] = useState('');
+
+    const sendMessage = async () => {
+        if (currentMessage) {
+            const messageData = {
+                room,
+                author: userName,
+                message: currentMessage,
+                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+            }
+        
+        const response = await fetch('https://node-server-test-one.vercel.app/webhook',{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(messageData)
+        })
+        response.json().then(data => console.log('test 11'))
+    }
+        
+    }
+    useEffect(() => {
+        console.log('socket changed')
+        socket.on("receive_message", (data) => {
+            console.log('testing msg',data)
+        });
+    }, [socket]);
+
+    return <div>
+        <div className="chat-header">
+            <p>Live Chat</p>
+        </div>
+        <div className="chat-body"></div>
+        <div className="chat-footer">
+            <input type="text" placeholder="Hey..." onChange={(e) => setCurrentMessage(e.target.value)} />
+        </div>
+        <button onClick={sendMessage}>&#9658;</button>
+    </div>;
+}
+
+export default Chat;
